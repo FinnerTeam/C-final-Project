f53766d9717550297203021345552d51d06f59a8
Elior Migdal <eliormi2@mta.ac.il>
2022-04-30 16:29:59 +0300

    Added tree functions (unfinished) & stash test

--- asdasd/ProjectMain.c
+++ asdasd/ProjectMain.c
@@ -4,6 +4,7 @@ void main()
 	FILE* InstrumentsData = NULL;
 	char** InstrumentsArr = NULL;
 	int mas;
+	int bla;
 	InstrumentsArr = FileToArr(InstrumentsData, InstrumentsArr);
 	heapSort(&InstrumentsArr, 13);
 	DeleteDuplication(InstrumentsArr, 13);
--- asdasd/TreeFunctions.c
+++ asdasd/TreeFunctions.c
@@ -1,1 +1,53 @@
 #include "ProjectHeader.h"
+
+TreeNode* createNewTreeNode(char* instrument, unsigned short insId, TreeNode* left, TreeNode* right)
+{
+    TreeNode* node = (TreeNode*)malloc(sizeof(TreeNode));
+    CheckMem(node);
+
+    int len = strlen(instrument);
+
+    node->instrument = (char*)malloc(sizeof(char) * len);
+    CheckMem(node->instrument);
+
+    node->insId = insId;
+    node->left = left;
+    node->right = right;
+}
+
+InstrumentTree BuildTreeFromArray(char** instrumentArr, int size)
+{
+    int i = 0;
+    return RecBuildTreeFromArray(instrumentArr + size / 2, size, &i);
+}
+
+InstrumentTree RecBuildTreeFromArray(char** instrumentArr, int size, int* insId)
+{
+    InstrumentTree output;
+
+    if (size == 0)
+    {
+        output.root = NULL;
+        return output;
+    }
+
+    else if (size == 1)
+    {
+        TreeNode* root = NULL, * leftNode = NULL, * rightNode = NULL;
+
+        leftNode = createNewTreeNode(*(instrumentArr - 1), (*insId) + 1, NULL, NULL);
+        rightNode = createNewTreeNode(*(instrumentArr + 1), (*insId) + 2, NULL, NULL);
+        root = createNewTreeNode(*instrumentArr, *insId, leftNode, rightNode);
+
+        *insId = *insId + 3;
+
+        output.root = root;
+        return output;
+    }
+
+    else
+    {
+        InstrumentTree leftTree, rightTree, output;
+        int i = 0;
+    }
+}
