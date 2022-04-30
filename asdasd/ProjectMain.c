e4fa4a3bdc72fb9ed0ed5aa5be90abae9fe81ad1
talpiven <80389574+talpiven@users.noreply.github.com>
2022-04-30 16:33:16 +0300

    testing stash

--- asdasd/ProjectMain.c
+++ asdasd/ProjectMain.c
@@ -4,7 +4,11 @@ void main()
 	FILE* InstrumentsData = NULL;
 	char** InstrumentsArr = NULL;
 	int mas;
+<<<<<<< Updated upstream
 	int bla;
+=======
+	int kas;
+>>>>>>> Stashed changes
 	InstrumentsArr = FileToArr(InstrumentsData, InstrumentsArr);
 	heapSort(&InstrumentsArr, 13);
 	DeleteDuplication(InstrumentsArr, 13);
